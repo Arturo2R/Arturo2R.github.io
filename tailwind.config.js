d0a0b804640d218ce4b352b9d251da945ec94aeb
Arturo Rebolledo <elrayoarturopro@outlook.com>
2021-07-06 18:38:17 -0500

    Perfecto para la version 1 Porfin sali de esto despues de un año Ya puedo reclamar mi diploma de Freecodecamp

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "royal-red": "#E63946",
+        "good-gray": "#96A6B8",
+        hueso: "#FAF9F9",
+        "bad-black": "#2A2938",
+        "dar-red": "#BB4E4D",
+      },
+      fontFamily: {
+        primary: "'Cinzel Decorative', serif",
+        secondary: "'Zilla Slab', serif",
+        normal: "'Poppins', sans-serif",
+      },
+    },
+  },
+};
